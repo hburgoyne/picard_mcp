469ba8dcda563d4aaa4679d46ad2eca299f27c7e
Hayden Burgoyne <hburgoyne.dev@gmail.com>
2025-05-21 15:40:05 -0700

    Updated admin panel to include profile. Completed Phase 2!

--- django_client/memory_app/admin.py
+++ django_client/memory_app/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
-from .models import OAuthToken, Memory
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import OAuthToken, Memory, UserProfile
+
+# Inline admin class for UserProfile
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+# Extend the existing UserAdmin to include UserProfile
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_bio')
+    
+    def get_bio(self, obj):
+        try:
+            return obj.profile.bio[:50] + '...' if len(obj.profile.bio) > 50 else obj.profile.bio
+        except UserProfile.DoesNotExist:
+            return ""
+    get_bio.short_description = 'Bio'
+    
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 @admin.register(OAuthToken)
 class OAuthTokenAdmin(admin.ModelAdmin):
